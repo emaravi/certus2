b7203743aba3aed49dcf1904671b9f4a42a326ce
emaravi <emaravi@gmail.com>
2022-10-29 12:41:43 -0500

    segundo commit

--- js/miscript.js
+++ js/miscript.js
@@ -1,3 +1,16 @@
+
+/*
 function f1(){
     alert("Hola Mundo");
+}
+*/
+
+/**
+ * Función que muestra un mensaje de alerta
+ * @param {string} msg Mensaje a mostrar
+ * @since 1.0
+ * @version 2.0
+ */
+function f1(msg){
+    alert(msg);
 }
